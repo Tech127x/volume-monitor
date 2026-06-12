df725f9b3817940848ebd6d07deaceab3b91cf54
Tech127x <tech127x@users.noreply.github.com>
2026-06-12 12:17:20 -0600

    test bump

--- src/volume_monitor/__init__.py
+++ src/volume_monitor/__init__.py
@@ -9,7 +9,7 @@ from .config import MonitorConfig
 from .monitors.app_knobs import AppKnobMonitor
 from .monitors.volume import VolumeMonitor
 
-__version__ = "1.0.0"
+__version__ = "1.0.34"
 __author__ = "Tech127x"
 __license__ = "MIT"
 
